ca7d4dca990351354e22dbf7d7cb15519bb73988
Jacob Burenstam <burenstam@gmail.com>
2018-09-29 23:59:05 +0200

    Added width option - Closes #6
    See https://github.com/ankane/chartkick/commit/abf33bf2f7b98a8a97390a0ffe54055ff8248426#diff-1ff6a980e429e846cc2a374a5fe812e5

--- lib/chartkick.ex
+++ lib/chartkick.ex
@@ -19,12 +19,13 @@ defmodule Chartkick do
   def chartkick_chart(klass, data_source, options \\ []) do
     id     = Keyword.get_lazy(options, :id, &UUID.uuid4/0)
     height = Keyword.get(options, :height, "300px")
+    width  = Keyword.get(options, :width, "100%")
     only   = Keyword.get(options, :only)
     case only do
-      :html   -> chartkick_tag(id, height)
+      :html   -> chartkick_tag(id, height, width)
       :script -> chartkick_script(klass, id, data_source, options_json(options))
       _       -> """
-                 #{ chartkick_tag(id, height) }
+                 #{ chartkick_tag(id, height, width) }
                  #{ chartkick_script(klass, id, data_source, options_json(options)) }
                  """
     end
@@ -40,8 +41,8 @@ defmodule Chartkick do
   EEx.function_from_string(
     :def,
     :chartkick_tag,
-    ~s[<div id="<%= id %>" style="height: <%= height %>; text-align: center; color: #999; line-height: <%= height %>; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>],
-    ~w(id height)a
+    ~s[<div id="<%= id %>" style="width: <%= width %>; height: <%= height %>; text-align: center; color: #999; line-height: <%= height %>; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>],
+    ~w(id height width)a
   )
 
   @options ~w(colors curve dataset decimal discrete donut download label legend library max messages min points prefix refresh stacked suffix thousands title xtitle xtype ytitle)a
